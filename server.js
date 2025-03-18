b6d995198a4c4df8b5f1422ac8eda9f2da4fce49
Wolfmen421 <portugal_2000f@hotmail.com>
2025-03-17 21:17:31 -0400

    Updating the project with the new changes

--- server.js
+++ server.js
@@ -8,13 +8,34 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req,res) => {
-    res.render("index.ejs");
+app.get('/', (req, res) => {
+    res.render('index.ejs', { page: 'login' });
+});
+
+app.get('/createAccount', (req, res) => {
+    res.render('createAccount.ejs', { page: 'createAccount' });
 });
 
 app.get('/dashboard', (req,res) =>{
-    res.render('dashboard.ejs');
+    res.render('dashboard.ejs',{ page: 'dashboard' });
 });
+
+app.get('/inventory', (req, res) => {
+    res.render('inventory.ejs', { page: 'inventory' });
+});
+
+app.get('/sales', (req, res) => {
+    res.render('sales.ejs', { page: 'sales' });
+});
+
+app.get('/expenses', (req, res) => {
+    res.render('expenses.ejs', { page: 'expenses' });
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings.ejs', { page: 'settings' });
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
